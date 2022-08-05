6182b0f7a476bf2f4021dd4ea86e2b9b44891141
Gustavo Silva <contatogustavosilva017@outlook.com>
2022-08-05 01:59:52 -0300

    Fim do Projeto
    - No Responsivity
    - No Routes
    - No Backend

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/style.css'
+import '../public/styles/style.css'
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,46 @@
-function Home(){
+function Home() {
     return (
-        <div >
-            <h1>Olá, OxeTech! ☕</h1>
-        </div>
+        <main>
+            <header>
+                <a href="#">
+                    <img src="/images/oxeTechMainLogo.png" alt="" />
+                </a>
+
+                <nav>
+                    <ul>
+                        <li>Início</li>
+                        <li>Cadastro</li>
+                        <li>Login</li>
+                        <li>Sobre</li>
+                    </ul>
+                </nav>
+            </header>
+
+            <main class="triangleFrame">
+                <img className="triangleImg" src="/images/triangleFrame.png" alt="" />
+
+                <div className="logoList">
+                    <ul>
+                        <li>
+                            <img src="/images/oxeTechLabLogo.png" className="labLogo" alt="" />
+                        </li>
+                        <li>
+                            <img src="/images/oxeTechHat.png" className="HatLogo" alt="" />
+                        </li>
+                        <li>
+                            <img src="/images/oxeTechWorkLogo.png" className="WorkLogo" alt="" />
+                        </li>
+                        <li>
+                            <img src="/images/oxeTechDin.png" className="DinLogo" alt="" />
+                        </li>
+                    </ul>
+                </div>
+
+                <aside className="alagoasContainer">
+                    <img src="/images/alagoas.png" className="alagoasLogo" alt="" />
+                </aside>
+            </main>
+        </main>
     )
 }
 
